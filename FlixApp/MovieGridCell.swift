a41cb35a1a6e496fcd0cfb764ad4cd952284c66e
JAVIER VILLA <5004073810@wce111-08.local>
2019-02-07 20:35:20 -0800

    Commit # 9

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by JAVIER VILLA on 2/7/19.
+//  Copyright © 2019 JAVIER VILLA. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
